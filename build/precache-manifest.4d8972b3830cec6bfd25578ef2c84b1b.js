d73f16e7e7acf933f26e92356d283aad0d3626bb
dongxiaofan <394276877@qq.com>
2020-01-06 12:56:52 +0800

    部署git page

---
+++ build/precache-manifest.4d8972b3830cec6bfd25578ef2c84b1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "430069def5a5c23709572604ee763f29",
+    "url": "./index.html"
+  },
+  {
+    "revision": "93f6e8939d2244e0423e",
+    "url": "./static/css/2.498266b3.chunk.css"
+  },
+  {
+    "revision": "50e5567e41e303498631",
+    "url": "./static/css/main.e2b8553d.chunk.css"
+  },
+  {
+    "revision": "93f6e8939d2244e0423e",
+    "url": "./static/js/2.aed95407.chunk.js"
+  },
+  {
+    "revision": "50e5567e41e303498631",
+    "url": "./static/js/main.d0a2a473.chunk.js"
+  },
+  {
+    "revision": "207cd9499118f2be537f",
+    "url": "./static/js/runtime-main.d611ebf2.js"
+  },
+  {
+    "revision": "0899ffa6d98e086a98b5c9282d123434",
+    "url": "./static/media/login-bg.0899ffa6.jpg"
+  }
+]);
